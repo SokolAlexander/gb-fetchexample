8497afd6f60233300e8936248cbad94679d20ff5
SokolAlexander <asokol1994@gmail.com>
2020-12-22 20:04:58 +0300

    Added class component

--- src/components/ArticlesList/ArticlesList.js
+++ src/components/ArticlesList/ArticlesList.js
@@ -1,17 +1,49 @@
-import React, { useCallback } from "react";
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect } from "react";
+import { useSelector, connect, useDispatch } from "react-redux";
 
 import Article from "../Article/Article";
 
 export default function ArticlesList() {
-  const articles = useSelector(state => state.articles.articles);
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.articles.articles);
+
+  useEffect(() => {
+    // TODO - dispatch getArticles
+  }, []);
 
   const renderArticle = useCallback(
     (article) => (
-      <Article title={article.title} text={article.text} image={article.img} key={article.id} />
+      <Article
+        title={article.title}
+        text={article.text}
+        image={article.img}
+        key={article.id}
+      />
     ),
     []
   );
 
   return <div>{articles.map(renderArticle)}</div>;
 }
+
+// для любителей классов
+// class ArticlesList extends React.Component {
+//   renderArticle = (article) => (
+//     <Article
+//       title={article.title}
+//       text={article.text}
+//       image={article.img}
+//       key={article.id}
+//     />
+//   );
+
+//   render() {
+//     return <div>{articles.map(this.renderArticle)}</div>;
+//   }
+// }
+
+// const mapStateToProps = (state) => ({
+//   articles: state.articles.articles
+// });
+
+// export default connect(mapStateToProps)(ArticlesList);
